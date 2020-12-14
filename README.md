d9561980d2cd999409be4d2cedf2a883147beaa6
James Hilliard <james.hilliard1@gmail.com>
2020-12-13 21:16:01 -0700

    Add firmware files

---
+++ README.md
@@ -0,0 +1,3 @@
+# proprietary_vendor_firmware
+
+Include vendor firmware in your android builds.... fuck updating your firmware separately
---
+++ bonito/config.mk
@@ -0,0 +1,15 @@
+FIRMWARE_IMAGES := \
+    abl \
+    aop \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    hyp \
+    keymaster \
+    modem \
+    qupfw \
+    tz \
+    xbl_config \
+    xbl
+
+AB_OTA_PARTITIONS += $(FIRMWARE_IMAGES)
